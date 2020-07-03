bb9cb9c8345fa012e301e480a4174ec175af37ae
bacil thalji <btbacil32@gmail.com>
2020-07-03 17:10:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -42,5 +42,7 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
+haha_array = ["chocolate","words","mom","house",]
+haha_array[1] = "arrays!"
 
 end
