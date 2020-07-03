d4b77675a3f49e4bcf2bee35554c34205004dcc4
bacil thalji <btbacil32@gmail.com>
2020-07-03 16:48:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,13 +9,11 @@ end
 def add_element_to_end_of_array(array, element)
   cars_array = ["BMW","Mercedes"]
   cars_array.push("arrays!")
-  
 end
 
 def add_element_to_start_of_array(array, element)
   first_words = ["hello","hola"]
   first_words.unshift("wow")
-  
 end
 
 def remove_element_from_end_of_array(array)
